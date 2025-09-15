a2cbd25faaaa8863b668665742576f4463ff2cf0
x1nigo <109367483+x1nigo@users.noreply.github.com>
2025-09-16 03:19:46 +0800

    Add files via upload

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,66 +1,119 @@
 import XMonad
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Loggers
+import qualified XMonad.StackSet as W
 
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks)
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
 
 import XMonad.Layout.Spacing
+import XMonad.Layout.Renamed
+import XMonad.Layout.LayoutModifier
 
--- main = xmonad $ xmobarProp $ myConfig
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Spiral
+
 main :: IO ()
 main = xmonad
-	 . ewmhFullscreen
-	 . ewmh
-	 . withEasySB (statusBarProp "xmobar ~/.config/xmobar/xmobarrc" (pure myXmobarPP)) defToggleStrutsKey
+     $ ewmhFullscreen
+     $ ewmh
+     $ withEasySB (statusBarProp "xmobar ~/.config/xmobar/xmobarrc" (pure myXmobarPP)) defToggleStrutsKey
 	 $ myConfig
+
+windowCount :: X (Maybe String)
+windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
 myXmobarPP :: PP
 myXmobarPP = def
 	{ ppSep             = " | "
+	, ppTitle           = white . wrap " " " " . shorten 60
 	, ppTitleSanitize   = xmobarStrip
-	, ppCurrent         = red . wrap " " "" . xmobarBorder "Bottom" "#87d7f7" 2
-	, ppHidden          = blue . wrap " " ""
-	, ppHiddenNoWindows = lowWhite . wrap " " ""
+	, ppCurrent         = blue . wrap " " " " . xmobarBorder "Bottom" "#dc2800" 3
+	, ppHidden          = white . wrap " " " "
+	, ppHiddenNoWindows = black . wrap " " " "
+	, ppLayout          = red . wrap " " " "
 	, ppUrgent          = red . wrap (yellow "!") (yellow "!")
-	, ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
-	, ppExtras          = [logTitles formatFocused formatUnfocused]
+	, ppExtras          = [windowCount]
+	, ppOrder           = \[ws, l, t, _] -> [ws, l, t]
 	}
 	where
-		formatFocused   = magenta . ppWindow
-		formatUnfocused = blue . ppWindow
-
-		ppWindow :: String -> String
-		ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
-
-		blue, lowWhite, magenta, red, white, yellow :: String -> String
-		magenta  = xmobarColor "#8787f7" ""
-		blue     = xmobarColor "#57d7f7" ""
-		white    = xmobarColor "#d7d7d7" ""
-		yellow   = xmobarColor "#ffa747" ""
+		black, red, green, yellow, blue, magenta, cyan, white :: String -> String
+		black    = xmobarColor "#373737" ""
 		red      = xmobarColor "#f75757" ""
-		lowWhite = xmobarColor "#ebdbb2" ""
+		green    = xmobarColor "#87d7a7" ""
+		yellow   = xmobarColor "#ffa747" ""
+		blue     = xmobarColor "#57d7f7" ""
+		magenta  = xmobarColor "#8787f7" ""
+		cyan     = xmobarColor "#87d7f7" ""
+		white    = xmobarColor "#ebdbb2" ""
 
 myConfig = def
 	{ modMask            = mod4Mask
+	, manageHook         = ( isFullscreen --> doFullFloat ) <+> manageDocks
 	, layoutHook         = myLayout
-	, normalBorderColor  = "#373737"
+	, normalBorderColor  = "#282828"
 	, focusedBorderColor = "#570000"
-	, borderWidth        = 2
+	, borderWidth        = 3
 	}
 	`additionalKeysP`
-		[ ("M-<Return>", spawn "st")
-		, ("M-d",        spawn "dmenu_run")
-		, ("M-w",        spawn "zen-browser")
-		, ("M-C-r",      spawn "xmonad --recompile" )
-		, ("M-S-r",      spawn "xmonad --restart" )
+		[ ("M-<Return>",              spawn "st")
+		, ("M-d",                     spawn "dmenu_run -h 26")
+		, ("M-r",                     spawn "st -e lfup")
+		, ("M-w",                     spawn "zen-browser")
+		, ("M-b",                     spawn "bookmarker")
+		, ("M-v",                     spawn "watchvid")
+		, ("M-x",                     spawn "setbg -d")
+		, ("M-S-x",                   spawn "setbg -x")
+		, ("M-'",                     spawn "st -n termfloat -f monospace:size=16 -g 50x20 -e bc -lq")
+		, ("M-<Insert>",              spawn "inserter")
+		, ("M-`",                     spawn "dmenumoji")
+		, ("M-<Backspace>",           spawn "systemmenu")
+
+		, ("<XF86AudioMute>",         spawn "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle")
+		, ("<XF86AudioMicMute>",      spawn "wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle")
+		, ("<XF86AudioRaiseVolume>",  spawn "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+")
+		, ("<XF86AudioLowerVolume>",  spawn "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-")
+
+		, ("<XF86MonBrightnessUp>",   spawn "brightnessctl s 5%+")
+		, ("<XF86MonBrightnessDown>", spawn "brightnessctl s 5%-")
+
+		, ("M-<F1>",                  spawn "readme")
+		, ("M-<F2>",                  spawn "fontwizard")
+		, ("M-<F3>",                  spawn "st -e pulsemixer")
+		, ("M-<F4>",                  spawn "selectdisplay")
+		, ("M-<F12>",                 spawn "xmonad --restart" )
+		, ("M-C-r",                   spawn "xmonad --recompile" )
+		, ("<Print>",                 spawn "printscreen")
+		, ("M-q",                     kill)
 		]
 
-myLayout = spacing 8 (tiled ||| Mirror tiled) ||| Full
-	where
-		tiled = Tall nmaster delta ratio
-		nmaster = 1
-		ratio = 1/2
-		delta = 3/100
+mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
+
+-- Define layouts for convenience and order
+tall     = renamed [Replace "tall"]
+		   $ mySpacing 6
+		   $ Tall 1 (3/100) (1/2)
+
+monocle  = renamed [Replace "monocle"]
+           $ smartBorders
+		   $ Full
+
+threeCol = renamed [Replace "threeCol"]
+           $ mySpacing 6
+		   $ ThreeCol 1 (3/100) (1/2)
+
+spirals  = renamed [Replace "spirals"]
+           $ mySpacing 6
+		   $ spiral (6/7)
+
+-- My main layout function
+myLayout = avoidStruts $ tall
+                     ||| spirals
+                     ||| threeCol
+                     ||| noBorders monocle
