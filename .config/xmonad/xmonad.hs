b92a6c13541e669aa6a63cddf3f910c32bf50d05
x1nigo <109367483+x1nigo@users.noreply.github.com>
2025-10-10 08:39:13 +0800

    Add files via upload

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -14,7 +14,11 @@ import XMonad.Util.ClickableWorkspaces -- requires `xdotool` in $PATH + UnsafeXM
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
+import System.Exit
+
 import XMonad.Actions.Promote
+import Data.Tree
+import XMonad.Actions.TreeSelect
 
 import XMonad.Hooks.StatusBar.PP (wrap, xmobarColor, xmobarBorder, xmobarPP, shorten, xmobarStrip, PP(..))
 import XMonad.Hooks.StatusBar
@@ -31,6 +35,7 @@ import XMonad.Layout.Spacing (spacingWithEdge, toggleWindowSpacingEnabled, toggl
 import XMonad.Layout.Renamed
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.ShowWName
 
 import XMonad.Layout.NoBorders (noBorders, lessBorders, Ambiguity(OnlyScreenFloat))
 import XMonad.Layout.ThreeColumns
@@ -71,7 +76,7 @@ myFocusedColor = "#d7d7f7"
 -- ==========
 
 -- myWorkspaces = [" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 "]
-myWorkspaces = [" www ", " dev ", " media ", " type ", " art ", " gfx ", " mus ", " sys ", " null "]
+myWorkspaces = ["www", "dev", "media", "typeW", "art", "sfx", "mus", "sys", "null"]
 
 -- =====
 -- Hooks
@@ -86,8 +91,10 @@ myManageHook = composeAll
     , className =? "Brave-browser"           --> doShift ( myWorkspaces !! 0 )
     , className =? "librewolf"               --> doShift ( myWorkspaces !! 0 )
     , className =? "firefox"                 --> doShift ( myWorkspaces !! 0 )
+    , className =? "mpv"                     --> doShift ( myWorkspaces !! 2 )
     , className =? "Gimp"                    --> doShift ( myWorkspaces !! 5 )
     , className =? "libreoffice-startcenter" --> doShift ( myWorkspaces !! 3 )
+    , className =? "Lxappearance"            --> doShift ( myWorkspaces !! 7 )
     ]
 
 -- ====
@@ -95,12 +102,7 @@ myManageHook = composeAll
 -- ====
 
 main :: IO ()
-main = do
-      xmonad
-    . ewmhFullscreen
-    . ewmh
-    . withEasySB mySB mySK -- outputs to `xmobar`
-    $ myConf
+main = xmonad . ewmhFullscreen . ewmh . withEasySB mySB mySK $ myConf
 
 mySB = statusBarProp "xmobar $HOME/.config/xmobar/xmobarrc" (clickablePP myPP)
 
@@ -112,8 +114,8 @@ myPP = def
     , ppTitle           = xmobarColor "#d7d7f7" "" . wrap " " " " . shorten 70
     , ppTitleSanitize   = xmobarStrip
     , ppCurrent         = xmobarColor "#5757f7" "" . wrap "[" "]"
-    , ppHidden          = xmobarColor "#ff8747" ""
-    , ppHiddenNoWindows = xmobarColor "#373737" ""
+    , ppHidden          = xmobarColor "#ff8747" "" . wrap " " " "
+    , ppHiddenNoWindows = xmobarColor "#373737" "" . wrap " " " "
     , ppLayout          = xmobarColor "#f74747" "" . wrap " " " "
     , ppUrgent          = xmobarColor "#f78747" "" . wrap "!" "!"
     , ppOrder           = \[ws,l,t] -> [ws,l,t]
@@ -137,6 +139,45 @@ myXPConfig = def
     , historySize         = 0
     , showCompletionOnTab = False
     }
+
+-- =========================
+-- Show WS names upon switch
+-- See `myLayoutHook`
+-- =========================
+
+mySWNConfig :: SWNConfig
+mySWNConfig = def
+    { swn_font    = "xft:monospace:bold:size=32"
+    , swn_bgcolor = "#21242b"
+    , swn_color   = "#d7d7f7"
+    , swn_fade    = 1
+    }
+
+myTreeConf :: TSConfig a
+myTreeConf = def
+    { ts_hidechildren = True
+    , ts_background   = 0xe721242b
+    , ts_font         = "xft:monospace-10"
+    , ts_node_width   = 200
+    , ts_node_height  = 24
+    , ts_node         = (0xffd7d7f7, 0xff21242b)
+    , ts_nodealt      = (0xffd7d7f7, 0xff1d2024)
+    , ts_highlight    = (0xff282828, 0xff5757d7)
+    }
+
+myActions =
+    [ Node (TSNode "XMonad" "Execute an xmonad-specific action" (return ()))
+	    [ Node (TSNode "Restart WM" "Restart xmonad" (spawn "xmonad --restart")) []
+	    , Node (TSNode "Recompile WM" "Recompile xmonad" (spawn "xmonad --recompile")) []
+	    , Node (TSNode "Logout/Quit" "Log (quit) out of xmonad" (io exitSuccess)) []
+        ]
+    , Node (TSNode "Applications" "Select the usual programs to run" (return ()))
+	    [ Node (TSNode "Terminal" "Open up the terminal" (spawn myTerminal)) []
+	    , Node (TSNode "Browser" "Search the internet (www)" (spawn myBrowser)) []
+	    , Node (TSNode "Image Editor" "Run an image-editing program" (spawn "gimp")) []
+	    , Node (TSNode "Office Suite" "Create/Edit documents" (spawn "libreoffice")) []
+        ]
+    ]
 
 -- =====================================
 -- Custom configuration and keybindings
@@ -190,6 +231,7 @@ myConf = def
         , ("M-p",                     shellPrompt myXPConfig)
         , ("M-S-p",                   xmonadPrompt myXPConfig)
         , ("M-m",                     manPrompt myXPConfig)
+        , ("M-S-t",                   treeselectAction myTreeConf myActions)
         , ("M-q",                     kill)
         ]
 
@@ -197,34 +239,34 @@ myConf = def
 -- Layouts
 -- =======
 
--- NOTE: changes to spacing, renaming, and ratio of layouts will probably require a reboot
+-- NOTE: changes to spacing, renaming, ratio, and other aspects of layouts will probably require a reboot
 
 tall      = renamed [Replace "tall"]
-            $ spacingWithEdge 8
+            $ spacingWithEdge 4
             $ Tall 1 (3/100) (1/2)
 
 stackT    = renamed [Replace "stackT"]
-            $ spacingWithEdge 8
+            $ spacingWithEdge 4
             $ StackTile 1 (3/100) (1/2)
 
 fibonacci = renamed [Replace "fibonacci"]
-            $ spacingWithEdge 8
+            $ spacingWithEdge 4
             $ spiral (6/7)
 
 threeCol  = renamed [Replace "threeCol"]
-            $ spacingWithEdge 8
+            $ spacingWithEdge 4
             $ ThreeCol 1 (3/100) (1/2)
 
 grid      = renamed [Replace "grid"]
-            $ spacingWithEdge 8
+            $ spacingWithEdge 4
             $ GridRatio (4/3)
 
 cmaster   = renamed [Replace "cmaster"]
-            $ spacingWithEdge 8
+            $ spacingWithEdge 4
             $ centerMaster Grid
 
 cmasterF  = renamed [Replace "cmasterF"]
-            $ spacingWithEdge 8
+            $ spacingWithEdge 4
             $ CenterMainFluid 1 (3/100) (70/100)
 
 monocle   = renamed [Replace "monocle"]
@@ -233,13 +275,13 @@ monocle   = renamed [Replace "monocle"]
 floating  = renamed [Replace "floating"]
             $ simplestFloat
 
-myLayoutHook = lessBorders OnlyScreenFloat $ avoidStruts $ toggleLayouts (noBorders monocle)
-                                                         $ tall      |||
-                                                           stackT    |||
-                                                           fibonacci |||
-                                                           threeCol  |||
-                                                           grid      |||
-                                                           cmaster   |||
-                                                           cmasterF  |||
-                                                 noBorders monocle   |||
-                                                           floating
+myLayoutHook = showWName' mySWNConfig $ lessBorders OnlyScreenFloat $ avoidStruts $ toggleLayouts (noBorders monocle)
+                                                                                  $ tall      |||
+                                                                                    stackT    |||
+                                                                                    fibonacci |||
+                                                                                    threeCol  |||
+                                                                                    grid      |||
+                                                                                    cmaster   |||
+                                                                                    cmasterF  |||
+                                                                          noBorders monocle   |||
+                                                                                    floating
